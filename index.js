a2cf38049463da9c5588a7c8849e4c78eace5892
sabiqun12 <sabiqun35@gmail.com>
2021-05-20 22:48:02 +0600

    work half done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+const fileUpload = require('express-fileupload');
+
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
@@ -8,6 +11,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('orders'));
+app.use(fileUpload());
+
 
 
 const port = 5000
@@ -22,7 +28,9 @@ app.get('/', (req, res) => {
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const serviceCollection = client.db("creativeAgency").collection("allService");
-    
+    const orderCollection = client.db("creativeAgency").collection("order");
+    const addServiceCollection = client.db("creativeAgency").collection("addService");
+    const adminCollection = client.db("creativeAgency").collection("admin");
 
     app.post('/addReview', (req, res) => {
         const review = req.body;
@@ -33,7 +41,91 @@ client.connect(err => {
             //console.log(result)
 
         })
-    })
+    });
+    app.get('/reviews', (req, res) => {
+        serviceCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+
+    });
+
+    // app.post('/addOrder', (req, res) => {
+    //     const order = req.body;
+    //     orderCollection.insertOne(order)
+    //     .then( result => {
+    //         res.send(result.insertedOne > 0)
+    //         console.log(result)
+    //     })
+    // });
+
+    app.post('/addOrder', (req, res) => {
+        const file = req.files.file;
+        const name = req.body.name;
+        const email = req.body.email;
+        const description = req.body.details;
+        const cost = req.body.price;
+        console.log(file,name,email,description, cost)
+        const filePath = `${__dirname}/orders/${file.name}`;
+        file.mv(filePath, err => {
+            if(err){
+                //console.log(err)
+                return res.status(5000).send({msg: 'faild to upload image'});
+            }
+            return res.send({name: file.name, path:`/${file.name}`})
+
+        })
+        orderCollection.insertOne({ name, email, description,file })
+        .then(result => {
+            //console.log(result)
+            res.send(result.insertedCount > 0)
+           // console.log(result)
+
+        })
+    });
+
+    app.get('/orders', (req, res) => {
+        orderCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+
+    });
+
+    app.post('/addService', (req, res) => {
+        const file = req.files.file;
+        const name = req.body.name;
+        const details = req.body.description;
+        
+        //console.log(file,name,details)
+        const filePath = `${__dirname}/service/${file.name}`;
+        file.mv(filePath, err => {
+            if(err){
+                //console.log(err)
+                return res.status(5000).send({msg: 'faild to upload image'});
+            }
+            return res.send({name: file.name, path:`/${file.name}`})
+
+        })
+        addServiceCollection.insertOne({ name,details,file })
+        .then(result => {
+            //console.log(result)
+            res.send(result.insertedCount > 0)
+           // console.log(result)
+
+        })
+    });
+
+    app.post('/addAdmin', (req, res) => {
+        const adminEmail = req.body;
+    
+        adminCollection.insertOne(adminEmail)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+            // console.log(result)
+
+        })
+    });
 
 
 });
